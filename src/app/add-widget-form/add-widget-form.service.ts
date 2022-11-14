8ff2081eb4f288a5503a02deda7ab918599e8e13
Antwan <antwansittard@gmail.com>
2022-11-14 10:46:53 +0100

    sent widget

--- src/app/add-widget-form/add-widget-form.service.ts
+++ src/app/add-widget-form/add-widget-form.service.ts
@@ -15,11 +15,29 @@ export class WidgetGridService{
 
   constructor(private http: HttpClient) {
   }
-  postWidget(data : widget){
-    let params = new HttpParams();
-    params = params.append('column', data.column);
-    params = params.append('row', data.row);
-    params = params.append('location', data.location);
-    this.http.post(this.url , "",{params: params});
+
+  async postWidget(data : widget){
+
+
+    const response = await fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'This will be the title',
+        body: 'Setting the body property',
+        column : data.column,
+        row : data.row,
+        location : data.location
+
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+    })
+
+    const dat = await response.json();
+    console.log(data);
+    console.log(dat);
+
   }
+
 }
