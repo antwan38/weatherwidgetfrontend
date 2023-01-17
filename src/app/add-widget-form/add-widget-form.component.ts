ac264ceb4ce07b79f8798d1c2f92185a2d390421
Antwan <antwansittard@gmail.com>
2023-01-17 12:23:03 +0100

    ux verbetering

--- src/app/add-widget-form/add-widget-form.component.ts
+++ src/app/add-widget-form/add-widget-form.component.ts
@@ -20,7 +20,7 @@ export class AddWidgetFormComponent implements OnInit {
   public formData : any = {location: "", wColumn: null, wRow: null}
   public userIsSignedIn : boolean = false;
   @Inject('ActivatedRoute') private route: ActivatedRoute
-  constructor(private grid: GridService, private widget: WidgetGridService, route: ActivatedRoute, private router: Router) {
+  constructor(private grid: GridService, private widgetService : WidgetService, private widget: WidgetGridService, route: ActivatedRoute, private router: Router) {
     this.route = route;
   }
 
@@ -42,26 +42,30 @@ export class AddWidgetFormComponent implements OnInit {
 
   }
 
-  postGrid(data : widget) :void{
+  postGrid(rawData : widget) :void{
     try {
-      if (this.id ===  null) {
-        this.widget.postWidget(data).then((data) => {
-          this.router.navigate(["/home"]);
-        });
-      } else {
-        if (data.location === "" || data.location === null){
-          data.location = this.formData.location;
+      this.widgetService.getWidget(rawData.location).then((data) => {
+        let widgetData : widget = rawData;
+        widgetData.location = data.name;
+        if (this.id === null) {
+          this.widget.postWidget(widgetData).then((data) => {
+            this.router.navigate(["/home"]);
+          });
+        } else {
+          if (data.location === "" || data.location === null) {
+            data.location = this.formData.location;
+          }
+          if (data.row === "" || data.row === null) {
+            data.row = this.formData.wRow;
+          }
+          if (data.column === "" || data.column === null) {
+            data.column = this.formData.wColumn;
+          }
+          this.widget.editWidget(widgetData, this.id).then((data) => {
+            this.router.navigate(["/home"]);
+          });
         }
-        if (data.row === "" || data.row === null){
-          data.row = this.formData.wRow;
-        }
-        if (data.column === "" || data.column === null){
-          data.column = this.formData.wColumn;
-        }
-        this.widget.editWidget(data, this.id).then((data) => {
-          this.router.navigate(["/home"]);
-        });
-      }
+      });
     }catch (e){
       console.log(e)
     }
